045503e84d643a994e29376f38fac9b06771b575
LiviuScrieCod <constantinliviudaniel@gmail.com>
2026-02-15 21:10:41 +0200

    getting ready to rethink project architecture, saving progress

--- projects/day_01/band_name_generator.py
+++ projects/day_01/band_name_generator.py
@@ -2,14 +2,7 @@ import random
 from collections import Counter
 from data import question_pool
 
-# question_set = random.sample(question_pool, random.randint(1, len(question_pool)))
-# print(question_set)
-# answer=''
 generator_session_log= []
-
-print("Welcome to the band name generator! type start")
-start=input("Welcome to the band name generator! type start")
-
 
 while True:
     answer=''
@@ -34,26 +27,3 @@ while True:
         again = input("\nGenerate one more name? y/n").lower()
     if again != 'y':
         break
-
-
-
-
-
-
-
-# for question in question_set:
-#     while True:
-#         user_input = input(question).strip()
-#         if len(user_input) > 0:
-#             answer= answer + user_input + ' '
-#             break
-#         else:
-#             print("One simply cannot write without using letters!")
-#
-# display_answer = 'The ' + answer.rstrip()
-# if (display_answer.endswith("s")):
-#     display_answer+="'"
-# else:
-#     display_answer+="s"
-# generator_session_log.append(display_answer)
-# print(display_answer)
